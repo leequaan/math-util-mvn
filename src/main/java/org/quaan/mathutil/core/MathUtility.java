04af2335207ac34cff3349c1c68e86df7a357a11
leequaan <l3qu4n1@gmail.com>
2023-06-26 16:50:41 +0700

    update code to use recursion and see green4ever

--- src/main/java/org/quaan/mathutil/core/MathUtility.java
+++ src/main/java/org/quaan/mathutil/core/MathUtility.java
@@ -20,14 +20,31 @@ public class MathUtility {
 //    21 giai thừa kiểu long chứa ko được
 //    hàm chỉ cho phép n = 0..20
 //    đưa n > 20; CHỬI, NÉM RA EXCEPTION
+    
+    //public static long getFactorial(int n) {
+        //if (n < 0 || n > 20) throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
+        //if (n == 0 || n == 1) return 1;
+        //long product = 1; // Biến gom các tích nhân với nhau
+        //for (int i = 2; i <= n; i++) 
+            //product *= i;
+        //return product;
+    //}
+    
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
         if (n == 0 || n == 1) return 1;
-        long product = 1; // Biến gom các tích nhân với nhau
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        return product;
+        
+        return n * getFactorial(n-1);
+        // đệ quy  recursion
+        
     }
+    
+    // 5! = 1.2.3.4.5 = 4!.5 = 5.4!
+    // 4! = 4.3!
+    // 3! = 3.2!
+    // 2! = 2.1!
+    // 1! = 1 stop
+    // n! = n.(n-1)!
 }
 
 // TDD: Test Driven Development là kĩ thuật viết code cí chất lượng bằng cách code và test case và test run được thực thi đan xen
